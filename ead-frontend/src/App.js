2f6c3dd894eaee7378c9c566e27815f7e4a1ca73
Kelum Ayuwardhana <kelumayuwardhana@gmail.com>
2023-10-09 18:25:09 +0530

    User management - get all admins

--- ead-frontend/src/App.js
+++ ead-frontend/src/App.js
@@ -3,11 +3,8 @@ import {BrowserRouter} from 'react-router-dom';
 import MainRoutes from "./MainRoutes";
 import {RequestContextProvider} from "./pages/services/RequestContext";
 import {UserContextProvider} from "./pages/services/UserContex";
-import {useEffect} from "react";
+
 function App() {
-    useEffect(() => {
-        console.log('aaa',process.env.REACT_APP_API_URL)
-    })
     return (
         <RequestContextProvider baseURL={process.env.REACT_APP_API_URL}>
             <UserContextProvider>
