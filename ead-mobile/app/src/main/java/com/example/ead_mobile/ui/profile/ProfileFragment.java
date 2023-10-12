b5b95693b239ae7e4d13c06c3f8753d893c81bdd
Kelum Ayuwardhana <kelumayuwardhana@gmail.com>
2023-10-12 22:12:49 +0530

    Add comments

--- ead-mobile/app/src/main/java/com/example/ead_mobile/ui/profile/ProfileFragment.java
+++ ead-mobile/app/src/main/java/com/example/ead_mobile/ui/profile/ProfileFragment.java
@@ -1,11 +1,10 @@
 package com.example.ead_mobile.ui.profile;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +15,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.ead_mobile.LoginActivity;
 import com.example.ead_mobile.databinding.FragmentProfileBinding;
